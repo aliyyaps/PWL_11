434f1f6b4eb151040ba39e16fd079fe7addfc396
aliyyaps <aliyyaputris23@gmail.com>
2022-04-27 09:50:54 +0700

    Prak 2 - Added Todos Function

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Todo;
 
 class User extends Authenticatable
 {
@@ -41,4 +42,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // Praktikum 2 (Langkah 6) - Menambahkan Fungsi Todos
+    public function todos()
+    {
+        return $this->hasMany(Todo::class);
+    }
 }
