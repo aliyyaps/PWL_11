9c506a66a0e521fcb30dceedff6b79998f197587
aliyyaps <aliyyaputris23@gmail.com>
2022-04-27 10:13:29 +0700

    Prak 2 - Added Route Resource

--- routes/api.php
+++ routes/api.php
@@ -24,4 +24,6 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    // Praktikum 2 (Langkah 14) - Added Route Resource
+    Route::apiResource('/todos', TodoController::class);
 });
