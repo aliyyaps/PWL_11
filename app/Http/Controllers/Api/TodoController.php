50c500560352b893418645cf0fd1936d7da088f3
aliyyaps <aliyyaputris23@gmail.com>
2022-04-27 10:11:46 +0700

    Prak 2 - Edit Function to CRUD on Todo Resource

--- app/Http/Controllers/Api/TodoController.php
+++ app/Http/Controllers/Api/TodoController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\TodoRequest;
 use App\Models\Todo;
 use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class TodoController extends Controller
 {
@@ -19,7 +21,11 @@ class TodoController extends Controller
      */
     public function index()
     {
-        //
+        $user = auth()->user();
+        $todos = Todo::with('user')
+            ->where('user_id', $user->id)
+            ->get();
+        return $this->apiSuccess($todos);
     }
 
     /**
@@ -30,7 +36,14 @@ class TodoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validated();
+
+        $user = auth()->user();
+        $todo = new Todo($request->all());
+        $todo->user()->associate($user);
+        $todo->save();
+
+        return $this->apiSuccess($todo->load('user'));
     }
 
     /**
@@ -41,7 +54,7 @@ class TodoController extends Controller
      */
     public function show(Todo $todo)
     {
-        //
+        return $this->apiSuccess($todo->load('user'));
     }
 
     /**
@@ -53,7 +66,13 @@ class TodoController extends Controller
      */
     public function update(Request $request, Todo $todo)
     {
-        //
+        $request->validated();
+        $todo->todo = $request->todo;
+        $todo->label = $request->label;
+        $todo->done = $request->done;
+        $todo->save();
+
+        return $this->apiSuccess($todo->load('user'));
     }
 
     /**
@@ -64,6 +83,14 @@ class TodoController extends Controller
      */
     public function destroy(Todo $todo)
     {
-        //
+        if (auth()->user()->id == $todo->user_id) {
+            $todo->delete();
+            return $this->apiSuccess($todo);
+        }
+
+        return $this->apiError(
+            'Unauthorized',
+            Response::HTTP_UNAUTHORIZED
+        );
     }
 }
